a9ebfe56102a179bf315a5630dffbde19336aba8
Samet Aylak <sametaylak29@gmail.com>
2022-02-13 00:38:59 +0300

    Update premake

--- premake5.lua
+++ premake5.lua
@@ -8,33 +8,35 @@ project "GLFW"
 
 	files
 	{
-		"include/GLFW/glfw3.h",
-		"include/GLFW/glfw3native.h",
-		"src/glfw_config.h",
-		"src/context.c",
-		"src/init.c",
-		"src/input.c",
-		"src/monitor.c",
-		"src/vulkan.c",
-		"src/window.c"
+		"%{prj.location}/src/context.c",
+		"%{prj.location}/src/egl_context.c",
+		"%{prj.location}/src/egl_context.h",
+		"%{prj.location}/src/init.c",
+		"%{prj.location}/src/input.c",
+		"%{prj.location}/src/internal.h",
+		"%{prj.location}/src/monitor.c",
+		"%{prj.location}/src/osmesa_context.c",
+		"%{prj.location}/src/osmesa_context.h",
+		"%{prj.location}/src/vulkan.c",
+		"%{prj.location}/src/window.c"
 	}
 
 	filter "system:macosx"
 		pic "On"
 
-		systemversion "latest"
-		
 		files
 		{
-			"src/cocoa_init.c",
-			"src/cocoa_joystick.c",
-			"src/cocoa_monitor.c",
-			"src/cocoa_time.c",
-			"src/posix_thread.c",
-			"src/cocoa_window.c",
-			"src/glx_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c"
+			"%{prj.location}/src/cocoa_init.m",
+			"%{prj.location}/src/cocoa_joystick.m",
+			"%{prj.location}/src/cocoa_joystick.h",
+			"%{prj.location}/src/cocoa_monitor.m",
+			"%{prj.location}/src/cocoa_platform.h",
+			"%{prj.location}/src/cocoa_time.c",
+			"%{prj.location}/src/cocoa_window.m",
+			"%{prj.location}/src/nsgl_context.m",
+			"%{prj.location}/src/nsgl_context.h",
+			"%{prj.location}/src/posix_thread.c",
+			"%{prj.location}/src/posix_thread.h"
 		}
 
 		defines
